491d6dba7ee936a44c8b21f4f5f6bd5c64654b14
Archana Nagaraja <johndoe@example.com>
2021-04-29 20:53:20 -0700

    define schema - query and functionlity

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,46 @@
+const { User, Book } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        me: async (parent, args, context) => {
+            if(context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+            
+            return userData;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+    
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+          
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        }
     }
+
+
   };
   
   module.exports = resolvers;

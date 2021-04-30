0c8fc8ca86b93025fb628ab9903fbb6353fc7666
Archana Nagaraja <johndoe@example.com>
2021-04-29 20:13:30 -0700

    integrate apollo server, schemas-define tyedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +33,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });

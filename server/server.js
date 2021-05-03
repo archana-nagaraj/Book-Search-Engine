4d0108522ebe742f024d17c7bee6ac1e062a94f5
Archana Nagaraja <johndoe@example.com>
2021-05-02 18:30:38 -0700

    added comments, added package.json details

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
+      // -- new log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
